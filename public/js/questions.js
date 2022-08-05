225849aec2fb2bdaf317470620349876a5cb8393
Chris Kratz <Ckratz17@gmail.com>
2022-08-05 12:50:37 -0400

    all functions working properly

--- public/js/questions.js
+++ public/js/questions.js
@@ -33,42 +33,34 @@ $(".next").click(function(){
     })
 })
 
-$(".previous").click(function() {
-    if(animating) return false;
-    animating = true
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+	
+	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	previous_fs.show(); 
 
-    current_fs = $(this).parent();
-    next_fs = $(this).parent().next();
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			scale = 0.8 + (1 - now) * 0.2;
+			left = ((1-now) * 50)+"%";
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		easing: 'easeInOutBack'
+	});
+});
 
-    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-    
-    previous_fs.show()
-
-    current_fs.animate({opacity: 0},
-        {
-            step: function(now, mx) {
-                scale = 0.8 + (1 - now) * 0.2;
-                left = ((1-now) * 50)+"%"
-                opacity = 1 - now;
-                current_fs.css({
-                    'left': left
-                })
-
-                previous_fs.css({
-                    'transform': 'scale('+scale+')',
-                    'opacity': opacity
-                })
-            },
-            duration: 800,
-            complete: function(){
-                current_fs.hide();
-                animating = false
-            },
-
-            easing: 'easeInOutBack'
-        })
-})
-
-$('.submit').click(function(){
-    return false
+$(".submit").click(function(){
+	return false;
 })

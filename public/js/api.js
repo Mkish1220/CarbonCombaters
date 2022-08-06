6f1b94a3f7bae34e84e760323ef98f63fdcc94f1
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-05 20:22:21 -0400

    took out token

--- public/js/api.js
+++ public/js/api.js
@@ -1,7 +1,7 @@
 var queryURL = "https://beta3.api.climatiq.io/estimate"
 
 fetch(queryURL, {
-    headers: { "authorization": " Bearer 2D3CHJ5CF7MGJYJ6YGK6J1T2DX55" },
+    headers: { },
   })
     .then(function (response) {
         console.log(response);

49f6da32aaa9a2332779784af873d9b18e50d115
MKish420 <meaghanmpeters@hotmail.com>
2022-08-06 01:29:38 -0400

    added user model and userroutes added user to index controller

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const carsRoutes = require('./carsRoutes');
 
+const userRoutes = require('./user-routes');
 
+router.use('/users', userRoutes);
 router.use('/cars', carsRoutes);
 
 

6d48ca7849acb4f3e20372c939503f1abb7869f6
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-06 12:04:39 -0400

    continued work on logic for sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,11 @@
-const signUpHandler = (event) => {
+const signUpHandler = async (event) => {
     event.preventDefault();
 
     const signName = document.querySelector("signName").value.trim();
     const signEmail = document.querySelector("signEmail").value.trim();
     const signPass = document.querySelector("signPass").value.trim();
+
+    if signName && signEmail && signPass
 }
 
 

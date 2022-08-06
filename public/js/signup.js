d8c3eca9bd13d21792cfc55e7eb8305306c878d1
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-06 17:48:10 -0400

    fixed error with browser login

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,15 @@
 const signUpFormHandler = async (event) => {
     event.preventDefault();
 console.log("hello")
-    const name = document.getElementById("signName").value.trim();
+    const username = document.getElementById("signName").value.trim();
     const email = document.getElementById("signEmail").value.trim();
     const password = document.getElementById("signPass").value.trim();
 
     // sends a post to create the user
-    if (name && email && password) {
-        const response = await fetch('/api/users', {
+    if (username && email && password) {
+        const response = await fetch('/api/users/sign', {
           method: 'POST',
-          body: JSON.stringify({name, email, password}),
+          body: JSON.stringify({username, email, password}),
           headers: {'Content-Type': 'application/json'},
         });
     

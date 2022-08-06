360fa2c232134c2210fbdae069ecb02b02645c34
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-06 17:28:34 -0400

    fixing post from the browser - had to comment out highcharts due to errors

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,9 @@
 const signUpFormHandler = async (event) => {
     event.preventDefault();
 console.log("hello")
-    const name = document.querySelector("signName").value.trim();
-    const email = document.querySelector("signEmail").value.trim();
-    const password = document.querySelector("signPass").value.trim();
+    const name = document.getElementById("signName").value.trim();
+    const email = document.getElementById("signEmail").value.trim();
+    const password = document.getElementById("signPass").value.trim();
 
     // sends a post to create the user
     if (name && email && password) {

7043c0d9f266bb720a1338ad4ce4f889fa447162
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-07 09:25:17 -0400

    added logout button to profile page

--- public/js/login.js
+++ public/js/login.js
@@ -4,6 +4,9 @@ const loginFormHandler = async (event) => {
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
+    //PW - the conditional here reads email and password, but the front end asks for username and pass word for the sign in.  
+    // PW - Maybe we should change the sign in to say login instead
+  
     if (email && password) {
       const response = await fetch('/api/users/login', {
         method: 'POST',
@@ -11,8 +14,10 @@ const loginFormHandler = async (event) => {
         headers: { 'Content-Type': 'application/json' },
       });
   
+      // PW - where is this supposed to be taking them? to their profile page based on user id?  i dont think we have that set up.  
       if (response.ok) {
-        document.location.replace('/profile:id');
+        // document.location.replace('/profile:id');
+        document.location.replace('/profile');
       } else {
         alert('Failed to log in.');
       }

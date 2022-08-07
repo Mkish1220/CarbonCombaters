c54954e0fcdca56ea7872359eb040ff0c7134654
Chris Kratz <Ckratz17@gmail.com>
2022-08-07 16:29:44 -0400

    added chart.js to the package json and created a helper out of the chartjs

---
+++ utils/helpers.js
@@ -0,0 +1,40 @@
+module.exports = {
+    make_chart: () => {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [newQuestions.miles, newQuestions.hours, newQuestions.nattie, newQuestions.liquid, newQuestions.fuel, newQuestions.recycle, newQuestions.bbq, newQuestions.phones, newQuestions.water, newQuestions.meat, newQuestions.average, newQuestions.nobeef, newQuestions.vegetarian, newQuestions],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+    }
+}

aaeab531dcab8f9642724bfb958fbd696ea4a289
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-06 20:39:33 -0400

    created table in DB and seeded

--- controllers/api/questionsRoutes.js
+++ controllers/api/questionsRoutes.js
@@ -1,19 +1,15 @@
 const router = require('express').Router();
-const User = require('../../models');
+const Questions = require('../../models');
 
-// CREATE new user
+// CREATE questions in DB
+
 router.post('/', async (req, res) => {
-  try {
-    const dbUserData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.loggedIn = true;
-
-      res.status(200).json(dbUserData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+    try {
+        const questionsData = await Questions.create(req.body);
+        res.status(200).json(questionsData);
+  
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });

5476d7c9a652fb554d357c1691c28ee76270945b
Lulu Kapita <lulukapita@gmail.com>
2022-08-07 13:44:25 -0400

    Update algorithms.js

--- public/js/algorithms.js
+++ public/js/algorithms.js
@@ -3,8 +3,8 @@ var carGas = document.getElementById("gasoline").value.trim();
 var carDiesel = document.getElementById("diesel").value.trim();
 var carElectric = document.getElementById("electric").value.trim();
 var milesWeekly = document.getElementById("miles").value.trim();
-var carMpg = document.getElementById("mpg").value.trim();
-var electricityInput = document.getElementById("electricity").value.trim();
+//var carMpg = document.getElementById("mpg").value.trim();
+var hourlyInput = document.getElementById("electricity").value.trim();
 var naturalGasInput = document.getElementById("naturalGas").value.trim();
 var fuelOilInput = document.getElementById("fuelOil").value.trim();
 var liquidpetrolInput = document.getElementById("petrol").value.trim();
@@ -18,9 +18,50 @@ var nobeefDiet = document.getElementById("nobeef").value.trim();
 var vegitarianDiet = document.getElementById("vegitarian").value.trim();
 var veganDiet = document.getElementById("vegan").value.trim();
 
+var carScore = '';
+var gasScore = '';
+var dieselScore = '';
+var electricScore = '';
+var milesScore = '';
+// var mpgScore = '';
+var electricityScore = '';
+var naturalGasScore = '';
+var fuelOilScore = '';
+var liquidPetrolScore = '';
+
 if (carInput === 0) {
     carScore = 0;
 } else {
-    carScore = carInput * .01018;
+    carScore = gasScore + dieselScore + carElectric;
 }
-console.log("Car score is: " + carScore);
+    gasScore = carGas * .008887;
+    dieselScore = carDiesel * .01018;
+    electricScore = carElectric * .003288;
+    console.log(carScore);
+
+if (milesWeekly === 0) {
+    milesScore = 0;
+} else {
+    milesScore = milesWeekly * .00000775;
+}
+    console.log(milesScore);
+
+// if (carMpg === 0) {
+//     mpgScore = 0;
+// } else {
+//     mpgScore = carMpg * .008887;
+// }
+//     console.log(mpgScore);
+
+if (hourlyInput === 0) {
+    electricityScore = 0;
+} else {
+    electricityScore = hourlyInput * 3.35996;
+}
+    console.log(electricityScore);
+
+if (naturalGasInput === 0) {
+    naturalGasScore = 0;
+} else {
+    naturalGasScore = ;
+}

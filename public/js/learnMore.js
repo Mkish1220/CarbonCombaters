64da35852ffb955844000cc14a6022d5f2ded270
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-08 14:59:27 -0400

    data is going to the DB now

--- public/js/learnMore.js
+++ public/js/learnMore.js
@@ -1,163 +1,163 @@
 // ========================== HIGH CHARTS PIE CHART FOR FOOD EMISSIONS=================================================//
-Highcharts.chart('containerA', {
-    chart: {
-        renderTo: "chart-A",
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    title: {
-        text: "GREENHOUSE GAS CONTRIBUTION BY FOOD TYPE IN THE AVERAGE DIET"
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    accessibility: {
-        point: {
-            valueSuffix: '%'
-        }
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-            }
-        }
-    },
-    series: [{
-        name: 'Brands',
-        colorByPoint: true,
-        data: [{
-            name: 'Meats',
-            y: 56.6,
-            sliced: true,
-            selected: true
-        }, {
-            name: 'Dairy',
-            y: 18.3
-        }, {
-            name: 'Beverages',
-            y: 5.9
-        }, {
-            name: 'Fish and Seafood',
-            y: 5.8
-        }, {
-            name: 'Eggs',
-            y: 2.8
-        }, {
-            name: 'Vegetables',
-            y: 2.6
-        }, {
-            name: 'Grain Products',
-            y: 2.1
-        }, {
-            name: 'Fruits',
-            y: 1.6
-        }, {
-            name: 'Other',
-            y: 4.3
-        }]
-    }]
-});
+// Highcharts.chart('containerA', {
+//     chart: {
+//         renderTo: "chart-A",
+//         plotBackgroundColor: null,
+//         plotBorderWidth: null,
+//         plotShadow: false,
+//         type: 'pie'
+//     },
+//     title: {
+//         text: "GREENHOUSE GAS CONTRIBUTION BY FOOD TYPE IN THE AVERAGE DIET"
+//     },
+//     tooltip: {
+//         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+//     },
+//     accessibility: {
+//         point: {
+//             valueSuffix: '%'
+//         }
+//     },
+//     plotOptions: {
+//         pie: {
+//             allowPointSelect: true,
+//             cursor: 'pointer',
+//             dataLabels: {
+//                 enabled: true,
+//                 format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+//             }
+//         }
+//     },
+//     series: [{
+//         name: 'Brands',
+//         colorByPoint: true,
+//         data: [{
+//             name: 'Meats',
+//             y: 56.6,
+//             sliced: true,
+//             selected: true
+//         }, {
+//             name: 'Dairy',
+//             y: 18.3
+//         }, {
+//             name: 'Beverages',
+//             y: 5.9
+//         }, {
+//             name: 'Fish and Seafood',
+//             y: 5.8
+//         }, {
+//             name: 'Eggs',
+//             y: 2.8
+//         }, {
+//             name: 'Vegetables',
+//             y: 2.6
+//         }, {
+//             name: 'Grain Products',
+//             y: 2.1
+//         }, {
+//             name: 'Fruits',
+//             y: 1.6
+//         }, {
+//             name: 'Other',
+//             y: 4.3
+//         }]
+//     }]
+// });
 // ========================== HIGH CHARTS PIE CHART FOR FOOD EMISSIONS=================================================//
 
 // ========================== HIGH CHARTS PIE CHART FOR VEHICLE EMISSIONS=================================================//
 
-Highcharts.chart('containerB', {
-    chart: {
-        renderTo: "chart-B",
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    title: {
-        text: "GREENHOUSE GAS CONTRIBUTION BY FOOD TYPE IN THE AVERAGE DIET"
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    accessibility: {
-        point: {
-            valueSuffix: '%'
-        }
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-            }
-        }
-    },
-    series: [{
-        name: 'TRANSPORTATION GREENHOUSE GASES',
-        colorByPoint: true,
-        data: [{
-            name: 'Passenger Car',
-            y: 41,
-            sliced: true,
-            selected: true,
-            color: "red"
-        }, {
-            name: 'Medium or Heavy Duty Truck',
-            y: 24,
-            color: "blue"
-        }, {
-            name: 'Light Duty Truck',
-            y: 17,
-            color: "pink"
+// Highcharts.chart('containerB', {
+//     chart: {
+//         renderTo: "chart-B",
+//         plotBackgroundColor: null,
+//         plotBorderWidth: null,
+//         plotShadow: false,
+//         type: 'pie'
+//     },
+//     title: {
+//         text: "GREENHOUSE GAS CONTRIBUTION BY FOOD TYPE IN THE AVERAGE DIET"
+//     },
+//     tooltip: {
+//         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+//     },
+//     accessibility: {
+//         point: {
+//             valueSuffix: '%'
+//         }
+//     },
+//     plotOptions: {
+//         pie: {
+//             allowPointSelect: true,
+//             cursor: 'pointer',
+//             dataLabels: {
+//                 enabled: true,
+//                 format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+//             }
+//         }
+//     },
+//     series: [{
+//         name: 'TRANSPORTATION GREENHOUSE GASES',
+//         colorByPoint: true,
+//         data: [{
+//             name: 'Passenger Car',
+//             y: 41,
+//             sliced: true,
+//             selected: true,
+//             color: "red"
+//         }, {
+//             name: 'Medium or Heavy Duty Truck',
+//             y: 24,
+//             color: "blue"
+//         }, {
+//             name: 'Light Duty Truck',
+//             y: 17,
+//             color: "pink"
          
-        }, {
-            name: 'Commercial Aircraft',
-            y: 7,
-            color: "green"
-        }, {
-            name: 'Other',
-            y: 5
-        }, {
-            name: 'Rail',
-            y: 2
-        }, {
-            name: 'Ships or Boats',
-            y: 2
-        }, {
-            name: 'Motorcycle or Bus',
-            y: 1,
-            color: "grey"
-        }]
-    }]
-});
+//         }, {
+//             name: 'Commercial Aircraft',
+//             y: 7,
+//             color: "green"
+//         }, {
+//             name: 'Other',
+//             y: 5
+//         }, {
+//             name: 'Rail',
+//             y: 2
+//         }, {
+//             name: 'Ships or Boats',
+//             y: 2
+//         }, {
+//             name: 'Motorcycle or Bus',
+//             y: 1,
+//             color: "grey"
+//         }]
+//     }]
+// });
 // ========================== SLIDESHOW JAVASCRIPT =================================================//
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
--- public/js/newQuestions.js
+++ public/js/newQuestions.js
@@ -4,23 +4,23 @@ const questionsFormHandler = async (event) => {
   console.log("inside questionsFormHandler")
   event.preventDefault();
   const carInput = document.getElementById("carInput").value.trim();
-    const gasoline = document.getElementById("gasoline").value.trim();
-    const diesel = document.getElementById("diesel").value.trim();
-    const electric = document.getElementById("electric").value.trim();
+    const gasoline = document.getElementById("gasoline").checked;
+    const diesel = document.getElementById("diesel").checked;
+    const electric = document.getElementById("electric").checked;
     const miles = document.getElementById("miles").value.trim();
     const hours = document.getElementById("hours").value.trim();
-    const nattie = document.getElementById("nattie").value.trim();
-    const liquid = document.getElementById("liquid").value.trim();
-    const fuel = document.getElementById("fuel").value.trim();
-    const recycle = document.getElementById("recycle").value.trim();
+    const nattie = document.getElementById("nattie").checked;
+    const liquid = document.getElementById("liquid").checked;
+    const fuel = document.getElementById("fuel").checked;
+    const recycle = document.getElementById("recycle").checked;
     const bbq = document.getElementById("bbq").value.trim();
     const phones = document.getElementById("phones").value.trim();
     const water = document.getElementById("water").value.trim();
-    const meat = document.getElementById("meat").value.trim();
-    const average = document.getElementById("average").value.trim();
-    const nobeef = document.getElementById("nobeef").value.trim();
-    const vegetarian = document.getElementById("vegetarian").value.trim();
-    const vegan = document.getElementById("vegan").value.trim();
+    const meat = document.getElementById("meat").checked;
+    const average = document.getElementById("average").checked;
+    const nobeef = document.getElementById("nobeef").checked;
+    const vegetarian = document.getElementById("vegetarian").checked;
+    const vegan = document.getElementById("vegan").checked;
     // if (electricity) 
     // {
     // const response = await fetch('/api/newquestions', {
@@ -38,15 +38,29 @@ const questionsFormHandler = async (event) => {
     //   }
     // }
     // sends a post to create the user
-    if (gasoline || diesel || electric || miles || hours || nattie || liquid || fuel || recycle || bbq || phones || water || meat || nobeef || vegetarian || vegan
+
+
+
+//                              FULL INPUT VALUES BELOW
+    // (gasoline || diesel || electric || miles || hours || nattie || liquid || fuel || recycle || bbq || phones || water || meat || nobeef || vegetarian || vegan
          
-        ) 
+    //   ) 
+    // body: JSON.stringify({gasoline, diesel, electric, miles , hours , nattie , liquid, fuel, recycle,
+    //   bbq, phones, water, meat, nobeef, vegetarian, vegan
+    //    }),
+
+
+
+    if (gasoline || diesel || electric || miles || hours || nattie || liquid || fuel || recycle || bbq || phones || 
+      water || meat || average || nobeef || vegetarian || vegan
+         
+       ) 
         {
         const response = await fetch('/api/newquestions', {
           method: 'POST',
           body: JSON.stringify({gasoline, diesel, electric, miles , hours , nattie , liquid, fuel, recycle,
-          bbq, phones, water, meat, nobeef, vegetarian, vegan
-           }),
+            bbq, phones, water, meat, average, nobeef, vegetarian, vegan
+            }),
           headers: {'Content-Type': 'application/json'},
         });
 

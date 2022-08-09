9c12ace091780d5138597cc37cbe607edccfeb17
DRCallaghan <dennis.callaghan87@gmail.com>
2022-08-09 13:47:24 -0400

    small fixes to learn more

--- public/js/profile.js
+++ public/js/profile.js
@@ -49,45 +49,45 @@ Highcharts.chart('container', {
     name: "Factors",
     colorByPoint: true,
     data: [{
-        name: "Transportation",
-        y: 27.3,
-        drilldown: "Transportation"
-      },
-      {
-        name: "Energy Industry",
-        y: 28.9,
-        drilldown: "Energy"
-      },
-      {
-        name: "Other Fuel Combustions",
-        y: 11.9,
-        drilldown: "Other Fuel Combustions"
-      },
-      {
-        name: "Manufacturing",
-        y: 10.3,
-        drilldown: "Manufacturing"
-      },
-      {
-        name: "Fugitive Emissions",
-        y: 4.7,
-        drilldown: "Fugitive Emissions"
-      },
-      {
-        name: "Industrial Processes",
-        y: 5.6,
-        drilldown: "Industrial Processes"
-      },
-      {
-        name: "Agriculture",
-        y: 9.2,
-        drilldown: null
-      }   ,
-         {
-        name: "Waste",
-        y: 2.0,
-        drilldown: null
-      }
+      name: "Transportation",
+      y: 27.3,
+      drilldown: "Transportation"
+    },
+    {
+      name: "Energy Industry",
+      y: 28.9,
+      drilldown: "Energy"
+    },
+    {
+      name: "Other Fuel Combustions",
+      y: 11.9,
+      drilldown: "Other Fuel Combustions"
+    },
+    {
+      name: "Manufacturing",
+      y: 10.3,
+      drilldown: "Manufacturing"
+    },
+    {
+      name: "Fugitive Emissions",
+      y: 4.7,
+      drilldown: "Fugitive Emissions"
+    },
+    {
+      name: "Industrial Processes",
+      y: 5.6,
+      drilldown: "Industrial Processes"
+    },
+    {
+      name: "Agriculture",
+      y: 9.2,
+      drilldown: null
+    },
+    {
+      name: "Waste",
+      y: 2.0,
+      drilldown: null
+    }
     ]
   }],
   drilldown: {
@@ -103,46 +103,46 @@ Highcharts.chart('container', {
     },
 
     series: [{
-        name: "Chrome",
-        id: "Chrome",
-       },
-      {
-        name: "Firefox",
-        id: "Firefox",
-        
-      },
-      {
-        name: "Internet Explorer",
-        id: "Internet Explorer",
-      },
-      {
-        name: "Safari",
-        id: "Safari",
-      },
-      {
-        name: "Edge",
-        id: "Edge",
-        },
-      {
-        name: "Opera",
-        id: "Opera",
-        data: [
-          [
-            "v50.0",
-            0.96
-          ],
-          [
-            "v49.0",
-            0.82
-          ],
-          [
-            "v12.1",
-            0.14
-          ]
+      name: "Chrome",
+      id: "Chrome",
+    },
+    {
+      name: "Firefox",
+      id: "Firefox",
+
+    },
+    {
+      name: "Internet Explorer",
+      id: "Internet Explorer",
+    },
+    {
+      name: "Safari",
+      id: "Safari",
+    },
+    {
+      name: "Edge",
+      id: "Edge",
+    },
+    {
+      name: "Opera",
+      id: "Opera",
+      data: [
+        [
+          "v50.0",
+          0.96
+        ],
+        [
+          "v49.0",
+          0.82
+        ],
+        [
+          "v12.1",
+          0.14
         ]
-      }
+      ]
+    }
     ]
-  }
+  }]
 });
 
 const delButtonHandler = async (event) => {
